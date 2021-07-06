2cff106a87accdf8520ed9de53dabbb881748c0c
Dominik Domagalski <domagalski.dominik@accenture.com>
2021-07-06 18:26:11 +0200

    Add button

---
+++ src/components/Button.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+import PropTypes from 'prop-types';
+import colors from '../utils/colors';
+
+const Button = styled.button`
+  background-color: ${colors.primary};
+  color: ${colors.textColor};
+  padding: 15px;
+  border-radius: 15px;
+  border: none;
+`;
+
+export default Button;
