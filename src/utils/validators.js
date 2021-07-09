85f0612078f55de5c6bd8bfc85c28980c45faa78
Dominik Domagalski <domagalski.dominik@accenture.com>
2021-07-09 19:20:04 +0200

    Fix validators

--- src/utils/validators.js
+++ src/utils/validators.js
@@ -8,16 +8,16 @@ const validators = (values) => {
 
   if (!values.category) errors.category = 'Required';
 
-  if (!values.category === consts.PIZZA) {
+  if (values.category === consts.PIZZA) {
     if (!values.no_of_slices) errors.no_of_slices = 'Required';
     if (!values.diameter) errors.diameter = 'Required';
   }
 
-  if (!values.category === consts.SOUP) {
+  if (values.category === consts.SOUP) {
     if (!values.spiciness) errors.spiciness = 'Required';
   }
 
-  if (!values.category === consts.SANDWICH) {
+  if (values.category === consts.SANDWICH) {
     if (!values.slices_of_bread) errors.slices_of_bread = 'Required';
   }
   return errors;
